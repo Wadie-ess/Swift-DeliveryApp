7477d3924eab17d2f142ac836416d80576349f14
Wadie-ess <walidehanin@gmail.com>
2023-03-20 08:41:14 +0000

    add models missig file

---
+++ DeliveryApp/models/CategorieModel.swift
@@ -0,0 +1,25 @@
+//
+//  CategorieModel.swift
+//  WeatherUI
+//
+//  Created by Ouadia Essendoubi on 12/23/22.
+//  Copyright © 2022 Ouadia Essendoubi. All rights reserved.
+//
+
+import Foundation
+
+
+public struct Categorie: Hashable {
+    let name:String
+    let imagePath:String
+}
+
+public struct Food:Hashable
+{
+    let name:String
+    let category:String
+    let subtiltle:String
+    let price:Double
+    let imagePath:String
+    let description:String
+}
